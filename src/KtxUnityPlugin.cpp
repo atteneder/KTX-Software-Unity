c165961e30b4ad50fb5bd37183c283c75dcea409
Andreas Atteneder <andreas.atteneder@gmail.com>
2022-09-27 11:38:30 +0200

    feat: WIP Vulkan GPU upload

--- src/KtxUnityPlugin.cpp
+++ src/KtxUnityPlugin.cpp
@@ -18,7 +18,12 @@
 
 #include <ktx.h>
 
-#include <math.h>
+#ifdef KTX_UNITY_FEATURE_VULKAN
+#include <vulkan/vulkan.h>
+#include <ktxvulkan.h>
+#include "IUnityGraphicsVulkan.h"
+#endif
+
 #include <unordered_map>
 
 #ifdef KTX_UNITY_FEATURE_OPENGL
@@ -98,6 +103,50 @@ OnRenderEvent(int eventID) {
         auto ktx = g_ktxTextureQueue.pop();
         ktxTextureResult result;
 
+#ifdef KTX_UNITY_FEATURE_VULKAN
+        if( s_DeviceType == kUnityGfxRendererVulkan )
+        {
+            auto m_UnityVulkan = s_UnityInterfaces->Get<IUnityGraphicsVulkan>();
+            auto m_Instance = m_UnityVulkan->Instance();
+
+            ktxVulkanDeviceInfo vdi;
+            ktxVulkanTexture texture;
+
+            VkCommandPool commandPool;
+            // Pool
+            VkCommandPoolCreateInfo cmdPoolInfo = {};
+            cmdPoolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+            cmdPoolInfo.queueFamilyIndex = 0; // todo : pass from example base / swap chain
+            cmdPoolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+            auto vkRes = vkCreateCommandPool(m_Instance.device, &cmdPoolInfo, nullptr, &commandPool);
+
+            if(vkRes==VK_SUCCESS) {
+
+                // Set up Vulkan physical device (gpu), logical device (device), queue
+                // and command pool. Save the handles to these in a struct called vkctx.
+                // ktx VulkanDeviceInfo is used to pass these with the expectation that
+                // apps are likely to upload a large number of textures.
+                ktxVulkanDeviceInfo_Construct(
+                        &vdi,
+                        m_Instance.physicalDevice,
+                        m_Instance.device,
+                        m_Instance.graphicsQueue,
+                        commandPool,
+                        nullptr
+                );
+
+                while (ktx) {
+                    ktxTexture_VkUpload(ktx.value(), &vdi, &texture);
+                    result.texture = (void*)texture.image;
+                    result.error = 0;
+                    g_ktxTextureResults[ktx.value()] = result;
+                    ktx = g_ktxTextureQueue.pop();
+                }
+
+                ktxVulkanDeviceInfo_Destruct(&vdi);
+            }
+        } else
+#endif
         if( s_DeviceType == kUnityGfxRendererOpenGLCore
             || s_DeviceType == kUnityGfxRendererOpenGLES20
             || s_DeviceType == kUnityGfxRendererOpenGLES30
